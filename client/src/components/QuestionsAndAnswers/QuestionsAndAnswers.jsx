95c7f0ddaef58a980758da6467eabf988485da49
Austin Pakola <austinpakola@gmail.com>
2022-02-09 16:08:09 -0600

    Initial skeleton built

---
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswers.jsx
@@ -0,0 +1,21 @@
+// Katie
+
+import React from "react";
+
+class QuestionsAndAnswers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1> Questions and Answers </h1>
+      </div>
+    )
+  }
+}
+
+export default QuestionsAndAnswers;
