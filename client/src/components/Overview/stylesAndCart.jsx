d11b556d16a8b88f807597845300bf4da349426c
Jesse Kovash <jessekovash@gmail.com>
2022-02-11 17:23:06 -0700

    finished building basic layout for overview component and its subcomponents

--- client/src/components/Overview/stylesAndCart.jsx
+++ client/src/components/Overview/stylesAndCart.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
 const StylesAndCart = function (props) {
-  return(
-    <div>
-      styles and cart
+  return (
+    <div className="styles-main-container">
+      <div className="styles-review-container">⭐️⭐️⭐️⭐️⭐️ Read all reviews</div>
+      <div className="styles-title-container">
+        <h4>Category: Shirts</h4>
+        <h2>Yellow shirt</h2>
+        <h6>$45</h6>
+      </div>
+      <div className="styles-colors-container">
+        <h2>Style > Selected Style</h2>
+        <div className="swatch-container">
+          <div className="one-swatch"></div>
+          <div className="one-swatch"></div>
+          <div className="one-swatch"></div>
+          <div className="one-swatch"></div>
+          <div className="one-swatch"></div>
+          <div className="one-swatch"></div>
+        </div>
+      </div>
+      <div className="styles-bag-container">
+        <div className="bag-top-container">
+          <select>
+            <option>XS</option>
+            <option>S</option>
+            <option>M</option>
+            <option>L</option>
+            <option>XL</option>
+          </select>
+          <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </div>
+        <div className="bag-bottom-container">
+         <button>Add to Bag +</button>
+         <button>Add to favorite</button>
+        </div>
+      </div>
     </div>
   )
 };
